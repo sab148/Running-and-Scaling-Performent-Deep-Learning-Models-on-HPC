3e11dcff5d9010cf47535b97cadafdf0351888f6
Jenya Sovetkin <e.sovetkin@gmail.com>
2026-01-20 09:46:38 +0100

    add HF training example

---
+++ train/hf_training.py
@@ -0,0 +1,143 @@
+import argparse
+import numpy as np
+import sys
+
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    Trainer,
+    TrainingArguments,
+    set_seed,
+)
+
+
+def get_model(name: str = "gpt2"):
+    # In HF, it's easy to load models for different downstream tasks:
+    #
+    # https://huggingface.co/docs/transformers/model_doc/auto
+    #
+    #
+    # For example, here we do text classification. More examples for NLP:
+    #
+    # https://huggingface.co/docs/transformers/v5.0.0rc1/en/model_doc/auto#natural-language-processing
+    #
+    model = AutoModelForSequenceClassification.from_pretrained(name, use_cache=False)
+    tokenizer = AutoTokenizer.from_pretrained(name)
+
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        model.config.pad_token_id = tokenizer.pad_token_id
+
+    return model, tokenizer
+
+
+def get_data(tokenizer,
+             name: str = "imdb",
+             nsample: int = 1000,
+             seed: int = 42):
+    # imdb dataset: sentiment analysis of movie reviews: text -> {0,1}
+    #
+    # https://huggingface.co/datasets/stanfordnlp/imdb
+
+    dataset = load_dataset(name)
+    d_train = dataset["train"]\
+        .shuffle(seed=seed)\
+        .select(range(nsample))
+    d_val = dataset["test"]\
+        .shuffle(seed=seed)\
+        .select(range(nsample))
+
+    d_train = d_train.map(
+        lambda x: tokenizer(
+            x['text'],
+            truncation=True,
+        ),
+        batched=True)
+    d_val = d_val.map(
+        lambda x: tokenizer(
+            x['text'],
+            truncation=True,
+        ),
+        batched=True)
+
+    return d_train, d_val
+
+
+def _opts():
+    p = argparse.ArgumentParser(
+        prog = sys.argv[0],
+        description = "Basic HF example")
+
+    p.add_argument(
+        '--training_type', '-t',
+        default = 'ddp',
+        choices = ('ddp', 'fsdp'),
+        help = 'type of training'
+    )
+
+    return p
+
+
+if '__main__' == __name__:
+    args = _opts().parse_args()
+    set_seed(42)
+
+    model, tokenizer = get_model()
+    d_train, d_val = get_data(tokenizer)
+
+    if 'ddp' == args.training_type:
+        # DDP parallelization (when on several nodes)
+        training_args = TrainingArguments(
+            output_dir = "./hf_example",
+            overwrite_output_dir = True,
+            save_strategy = 'no',
+            num_train_epochs = 3,
+            max_steps = 10,
+            learning_rate = 2e-5,
+            per_device_train_batch_size = 4,
+            bf16 = True,
+            accelerator_config = dict(
+                dispatch_batches = False,
+            ),
+        )
+    elif 'fsdp' == args.training_type:
+        # FSDP parallelization
+        training_args = TrainingArguments(
+            output_dir = "./hf_example",
+            overwrite_output_dir = True,
+            save_strategy = 'no',
+            num_train_epochs = 3,
+            max_steps = 10,
+            learning_rate = 2e-5,
+            per_device_train_batch_size = 4,
+            bf16 = True,
+            accelerator_config = dict(
+                dispatch_batches = False,
+            ),
+            fsdp = "full_shard auto_wrap",
+            fsdp_config = dict(
+                fsdp_activation_checkpointing = True
+            ),
+            gradient_checkpointing = False,
+        )
+    else:
+        raise NotImplemented(f"unknown --training_type={args.training_type}")
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=d_train,
+        eval_dataset=d_val,
+        processing_class=tokenizer,
+        data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
+    )
+
+    trainer.train()
+
+    # for more examples, see https://gitlab.jsc.fz-juelich.de/sdlaml/hf_llm
+    #
+    # everything is not that simple. Some options are implemented only for
+    # forward-passes only (i.e. for inference)
