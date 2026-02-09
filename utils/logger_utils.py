c4cb74cd5e446106db9dbe6943216035f0e4fcfc
benassou1 <benassou1@jrlogin05.jureca>
2026-02-09 14:47:24 +0100

    fixed bug for wandb

--- utils/logger_utils.py
+++ utils/logger_utils.py
@@ -1,6 +1,5 @@
 import torch
 import wandb
-import torch
 from torch.utils.tensorboard import SummaryWriter
 
 class LoggerUtils:
@@ -8,42 +7,41 @@ class LoggerUtils:
         self.logger = logger
         self.writer = None
         self.wandb = None
+        
         if (
-                (torch.distributed.is_initialized() and torch.distributed.get_rank() == 0)
-                or (not torch.distributed.is_initialized())
-            ):
+            (torch.distributed.is_initialized() and torch.distributed.get_rank() == 0)
+            or (not torch.distributed.is_initialized())
+        ):
             if logger == 'tensorboard':    
                 self.writer = SummaryWriter("tensorboard_logs")
             elif logger == 'wandb':            
                 # Initialize Weights & Biases
-                self.wandb.init(project="wandb_distributed_training",
-                            name=f"single_gpu_training_run",
-                            reinit=True)
-                self.wandb.config.update({"learning_rate": lr,
-                                    "epochs": epochs,
-                                    "batch_size": batch_size})
-
-
+                wandb.init(project="wandb_distributed_training",  # Changed from self.wandb.init
+                           name=f"single_gpu_training_run",
+                           mode="offline",
+                           reinit=True)
+                wandb.config.update({"learning_rate": lr,
+                                     "epochs": epochs,
+                                     "batch_size": batch_size})
+                self.wandb = wandb  # Store the wandb module for later use
+    
     def log_metrics(self, train_loss, val_loss, epoch):
         if (
-                (torch.distributed.is_initialized() and torch.distributed.get_rank() == 0)
-                or (not torch.distributed.is_initialized())
-            ):
+            (torch.distributed.is_initialized() and torch.distributed.get_rank() == 0)
+            or (not torch.distributed.is_initialized())
+        ):
             if self.logger == 'tensorboard':
-                # Log metrics to TensorBoard
                 self.writer.add_scalar('Loss/train', train_loss, epoch)
                 self.writer.add_scalar('Loss/val', val_loss, epoch)
             elif self.logger == 'wandb':
-                # Log metrics to Weights & Biases
-                self.wandb.log({"Loss/Train": train_loss, "Loss/Validation": val_loss, "Epoch": epoch})
-
-
+                wandb.log({"Loss/Train": train_loss, "Loss/Validation": val_loss, "Epoch": epoch})
+    
     def close_logger(self):
         if (
-                (torch.distributed.is_initialized() and torch.distributed.get_rank() == 0)
-                or (not torch.distributed.is_initialized())
-            ):  
+            (torch.distributed.is_initialized() and torch.distributed.get_rank() == 0)
+            or (not torch.distributed.is_initialized())
+        ):  
             if self.logger == 'tensorboard':
                 self.writer.close()
             elif self.logger == 'wandb':
-                self.wandb.finish()
+                wandb.finish()
